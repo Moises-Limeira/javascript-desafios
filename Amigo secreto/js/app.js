17e9cd350f165847ec963712d5c1c22e57e66b7b
Moises Limeira <limeir17@yahoo.com>
2024-08-25 23:43:19 -0300

    Projeto finalizado

--- Amigo secreto/js/app.js
+++ Amigo secreto/js/app.js
@@ -1,3 +1,58 @@
-function (params) {
+let amigos = [];
+function adicionar() {
+    let nomeAmigo = document.getElementById('nome-amigo').value; 
+    if (nomeAmigo == ''){
+        alert('Digite o nome do amigo!');
+        return;
+    }
+
+    if (amigos.includes(nomeAmigo)) {
+        alert('Nome já adicionado!');
+        return;
+    }
+
+    let incluirAmigos = document.getElementById('lista-amigos');
+
+    amigos.push(nomeAmigo);
+
+    if (incluirAmigos.textContent == ''){
+        incluirAmigos.textContent = nomeAmigo;
+    }else{
+        incluirAmigos.textContent = incluirAmigos.textContent + ', ' + nomeAmigo;
+    }
+
+    document.getElementById('nome-amigo').value = '';
     
+}
+
+function sortear(){
+    if (amigos.length < 4) {
+        alert('Adicione pelo menos 4 amigos!');
+        return;
+    }
+    
+    embaralha(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+    for (let i = 0; i < amigos.length; i++) {
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] + '<br>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] + '<br>';
+            }
+    }
+}
+
+function embaralha(lista) {
+    
+    for (let i = lista.length; i; i--){
+        let indiceAleatorio = Math.floor(Math.random() * i);
+
+        [lista[i - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[i - 1]];
+    }
+}
+
+function reiniciar() {
+    amigos = []
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
 }
